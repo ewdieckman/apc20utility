bdc1a9c56921f1307b9835a2e24624168ccc4de8
Dieckman, Eric <eric.dieckman@wisc.edu>
2020-03-03 19:37:26 -0600

    update assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MIDI SysEx")]
+[assembly: AssemblyTitle("APC20 Utility")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MIDI SysEx")]
+[assembly: AssemblyCompany("Acid Creek, LLC")]
+[assembly: AssemblyProduct("APC20 Utility")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
